4208eb793d5ccca4f94b4c857be2d8a2f281542b
Colman Humphrey <colmanhumphrey@gmail.com>
2020-10-06 08:23:21 -0400

    our first attempt at passing in db connections to run etc
    - run takes a PgConnection as input, passes this to application data
    - subscriptions intends to use this
    - main uses config now
    - need uuid and chrono crates

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
 use zero2prod::configuration;
 use zero2prod::startup;
@@ -7,7 +8,11 @@ async fn main() -> std::io::Result<()> {
     let configuration = configuration::get_configuration().expect("Failed to read configuration.");
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
+    let connection_string = configuration.database.connection_string();
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
 
     let listener = TcpListener::bind(address)?;
-    startup::run(listener)?.await
+    startup::run(listener, connection)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,13 +1,17 @@
 use crate::routes;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::PgConnection;
 use std::net::TcpListener;
+use std::sync::Arc;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+    let connection = Arc::new(connection);
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(routes::health_check))
             .route("/subscriptions", web::post().to(routes::subscribe))
+            .data(connection.clone())
     })
     .listen(listener)?
     .run();
