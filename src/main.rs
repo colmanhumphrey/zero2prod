02664348752df4e9e216faee6a8362d13f054358
Colman Humphrey <colmanhumphrey@gmail.com>
2020-10-08 11:07:44 -0400

    main uses env_logger
    - defaults to "info" if unset
    - had to change return type of main
    - so changing ending too

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,12 @@ use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration;
 use zero2prod::startup;
+use env_logger::Env;
 
 #[actix_rt::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> Result<(), anyhow::Error> {
+    env_logger::from_env(Env::default().default_filter_or("info")).init();
+
     let configuration = configuration::get_configuration().expect("Failed to read configuration.");
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
@@ -13,5 +16,7 @@ async fn main() -> std::io::Result<()> {
         .expect("Failed to connect to Postgres.");
 
     let listener = TcpListener::bind(address)?;
-    startup::run(listener, connection)?.await
+    startup::run(listener, connection)?.await?;
+
+    Ok(())
 }
