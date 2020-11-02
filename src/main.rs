9d01e41539166f9f4a5fa0060345e10482639a0f
Colman Humphrey <colmanhumphrey@gmail.com>
2020-11-02 16:42:04 -0500

    removing anyhow for now

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 // use env_logger::Env;
-use anyhow::Context;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::{configuration, startup, telemetry};
 
 #[actix_rt::main]
-async fn main() -> Result<(), anyhow::Error> {
+async fn main() -> std::io::Result<()> {
     let subscriber = telemetry::get_subscriber("zero2prod".into(), "info".into());
     telemetry::init_subscriber(subscriber);
 
@@ -16,6 +15,9 @@ async fn main() -> Result<(), anyhow::Error> {
         .await
         .map_err(anyhow::Error::from)
         .with_context(|| "Failed to connect to Postgres.")?;
+        .expect("Failed to connect to postgres");
+        // .map_err(anyhow::Error::from)
+        // .with_context(|| "Failed to connect to Postgres.")?;
 
     let listener = TcpListener::bind(address)?;
     startup::run(listener, connection)?.await?;
