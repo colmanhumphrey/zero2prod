4fd598b10888e8baf586783183bac4169d7ddf2b
Colman Humphrey <colmanhumphrey@gmail.com>
2020-11-03 15:19:32 -0500

    making application a full struct in configuration
    - instead of just a port, we have a host and a port
    - in main.rs we need to pass both along

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,11 @@ async fn main() -> std::io::Result<()> {
 
     let configuration = configuration::get_configuration().expect("Failed to read configuration.");
 
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host,
+        configuration.application.port,
+    );
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to postgres");
