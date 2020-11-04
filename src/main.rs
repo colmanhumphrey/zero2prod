ea0d33a661a040fa698664af5ddeaaf04c223498
Colman Humphrey <colmanhumphrey@gmail.com>
2020-11-04 17:00:25 -0500

    ah we're not using PgPool
    - completely replaced by PgPoolOptions, which makes sense

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 // use env_logger::Env;
-use sqlx::{postgres::PgPoolOptions, PgPool};
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use zero2prod::{configuration, startup, telemetry};
 
