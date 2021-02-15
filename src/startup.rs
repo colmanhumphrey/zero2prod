04ddcec86b8b7dc2422b41306cac00f2aae1d652
Colman Humphrey <colmanhumphrey@gmail.com>
2021-02-15 16:55:03 -0500

    extract most of main into startup
    - so we don't replicate it in tests
    - better also because now we can actually test this code!
    - need to return the port too for use in tests...

--- src/startup.rs
+++ src/startup.rs
@@ -3,9 +3,67 @@ use crate::routes;
 use actix_web::dev::Server;
 // use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
-use sqlx::PgPool;
+use sqlx::{postgres::PgPoolOptions, PgPool};
 use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
+
+use crate::{configuration, email_client};
+
+pub struct Application {
+    port: u16,
+    server: Server,
+}
+
+impl Application {
+    pub async fn build(configuration: configuration::Settings) -> Result<Self, std::io::Error> {
+        let connection_pool = get_connection_pool(&configuration.database)
+            .await
+            .expect("Failed to connect to Postgres.");
+
+        let sender_email = configuration
+            .email_client
+            .sender()
+            .expect("invalid sender email address");
+        let email_client = email_client::EmailClient::new(
+            configuration.email_client.base_url,
+            sender_email,
+            configuration.email_client.authorization_token,
+        );
+
+        let address = format!(
+            "{}:{}",
+            configuration.application.host, configuration.application.port,
+        );
+
+        let listener = TcpListener::bind(&address)?;
+        let port = listener.local_addr().unwrap().port();
+        // let address = if configuration.application.port == 0 {
+        //     listener.local_addr().unwrap().to_string()
+        // } else {
+        //   address
+        // };
+        let server = run(listener, connection_pool, email_client)?;
+
+        Ok(Self { port, server })
+    }
+
+    pub fn port(&self) -> u16 {
+        self.port
+    }
+
+    pub async fn run_until_stopped(self) -> Result<(), std::io::Error> {
+        self.server.await
+    }
+}
+
+pub async fn get_connection_pool(
+    configuration: &configuration::DatabaseSettings,
+) -> Result<PgPool, sqlx::Error> {
+    PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect_with(configuration.with_db())
+        .await
+}
 
 pub fn run(
     listener: TcpListener,
@@ -16,7 +74,6 @@ pub fn run(
     let email_client = web::Data::new(email_client);
     let server = HttpServer::new(move || {
         App::new()
-            // .wrap(Logger::default())
             .wrap(TracingLogger)
             .route("/health_check", web::get().to(routes::health_check))
             .route("/subscriptions", web::post().to(routes::subscribe))
