3c2ab3eb31f419b20630147d187b42e6231f212e
Colman Humphrey <colmanhumphrey@gmail.com>
2021-03-21 17:58:13 -0400

    adding tests for links and status, passing those tests

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -45,23 +45,48 @@ pub async fn subscribe(
         .await
         .map_err(|_| HttpResponse::InternalServerError().finish())?;
 
-    let _ = email_client
-        .send_email(
-            new_subscriber.email,
-            "Welcome!",
-            "Welcome to our newsletter!",
-            "Welcome to our newsletter!",
-        )
-        .await;
+    // for now ignoring error
+    let _ = send_confirmation_email(&email_client, new_subscriber).await;
 
     Ok(HttpResponse::Ok().finish())
 }
 
 #[tracing::instrument(
+    name = "Send a confirmation email to a new subscriber",
+    skip(email_client, new_subscriber)
+)]
+pub async fn send_confirmation_email(
+    email_client: &EmailClient,
+    new_subscriber: NewSubscriber,
+) -> Result<(), reqwest::Error> {
+    let confirmation_link = "https://my-api.com/subscriptions/confirm";
+
+    let plain_body = format!(
+        "Welcome to our newsletter!\nVisit {} to confirm your subscription.",
+        confirmation_link
+    );
+
+    let html_body = format!(
+        "Welcome to our newsletter!<br />\
+         Click <a href=\"{}\">here</a> to confirm your subscription.",
+        confirmation_link
+    );
+
+    email_client
+        .send_email(
+            new_subscriber.email,
+            "Welcome!",
+            &html_body,
+            &plain_body,
+        )
+        .await
+}
+
+
+#[tracing::instrument(
     name = "Saving new subscriber details in the database",
     skip(new_subscriber, pool)
 )]
-
 pub async fn insert_subscriber(
     pool: &PgPool,
     new_subscriber: &NewSubscriber,
@@ -69,7 +94,7 @@ pub async fn insert_subscriber(
     sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at, status)
-        VALUES ($1, $2, $3, $4, 'confirmed')
+        VALUES ($1, $2, $3, $4, 'pending_confirmation')
         "#,
         Uuid::new_v4(),
         new_subscriber.email(),
--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -11,14 +11,23 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     let response = app.post_subscriptions(body.into()).await;
 
     assert_eq!(200, response.status().as_u16());
+}
 
-    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+#[actix_rt::test]
+async fn subscribe_persists_the_new_subscriber() {
+    let app = spawn_app().await;
+    let body = "name=col%20man&email=colo%40col.com";
+
+    app.post_subscriptions(body.into()).await;
+
+    let saved = sqlx::query!("SELECT email, name, status FROM subscriptions",)
         .fetch_one(&app.db_pool)
         .await
         .expect("Failed to fetch saved subscription.");
 
     assert_eq!(saved.email, "colo@col.com");
     assert_eq!(saved.name, "col man");
+    assert_eq!(saved.status, "pending_confirmation");
 }
 
 #[actix_rt::test]
@@ -73,12 +82,31 @@ async fn subscribe_sends_a_confirmation_email_for_valid_data() {
     Mock::given(path("/email"))
         .and(method("POST"))
         .respond_with(ResponseTemplate::new(200))
-        .expect(1)
+        // testing for e.g. receiving stuff elsewhere,
+        // so dropping test here
         .mount(&app.email_server)
         .await;
 
     // Act
     app.post_subscriptions(body.into()).await;
 
-    // Assert on drop
+    // Assert
+    // Get first request
+    let email_request = &app.email_server.received_requests().await.unwrap()[0];
+
+    // parse body into JSON (from raw bytes)
+    let body: serde_json::Value = serde_json::from_slice(&email_request.body).unwrap();
+
+    let get_link = |s: &str| {
+        let links: Vec<_> = linkify::LinkFinder::new()
+            .links(s)
+            .filter(|l| *l.kind() == linkify::LinkKind::Url)
+            .collect();
+        assert_eq!(links.len(), 1);
+        links[0].as_str().to_owned()
+    };
+
+    let html_link = get_link(body["HtmlBody"].as_str().unwrap());
+    let text_link = get_link(body["TextBody"].as_str().unwrap());
+    assert_eq!(html_link, text_link);
 }
