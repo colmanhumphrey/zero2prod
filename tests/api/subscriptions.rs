a7f220ce0bb1a10d4d41c9d74899b80cc9580ec5
Colman Humphrey <colmanhumphrey@gmail.com>
2021-06-05 16:35:19 -0400

    adding test to check errors on fatal db error
    - mostly for the logs in fact!

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -103,3 +103,21 @@ async fn subscribe_sends_a_confirmation_email_for_valid_data() {
 
     assert_eq!(confirmation_links.html, confirmation_links.plain_text);
 }
+
+#[actix_rt::test]
+async fn subscribe_fails_if_there_is_a_fatal_database_error() {
+    let app = spawn_app().await;
+    let body = "name=colo%20man&email=man@colog.com";
+
+    // sabotage db:
+    sqlx::query!("ALTER TABLE subscription_tokens DROP COLUMN subscription_token;", )
+        .execute(&app.db_pool)
+        .await
+        .unwrap();
+
+    // Act
+    let response = app.post_subscriptions(body.into()).await;
+
+    // Assert
+    assert_eq!(response.status().as_u16(), 500);
+}
